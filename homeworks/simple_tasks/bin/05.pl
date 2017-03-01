28913d0e0f58c6029dd04294cd58e9ca4462c27b
MarinaSolyanikova <Solyanikova_marina@mail.ru>
2017-03-02 01:02:31 +0300

    Update 05.pl

--- homeworks/simple_tasks/bin/05.pl
+++ homeworks/simple_tasks/bin/05.pl
@@ -3,38 +3,19 @@
 use strict;
 use warnings;
 
-=encoding UTF8
-=head1 SYNOPSYS
-
-Поиск количества вхождений строки в подстроку.
-
-=head1 run ($str, $substr)
-
-Функция поиска количества вхождений строки $substr в строку $str.
-Пачатает количество вхождений в виде "$count\n"
-Если вхождений нет - печатает "0\n".
-
-Примеры: 
-
-run("aaaa", "aa") - печатает "2\n".
-
-run("aaaa", "a") - печатает "4\n"
-
-run("abcab", "ab") - печатает "2\n"
-
-run("ab", "c") - печатает "0\n"
-
-=cut
-
 sub run {
+
     my ($str, $substr) = @_;
     my $num = 0;
 
-    # ...
-    # Вычисление количества вохождений строки $substr в строку $str,
-    # ...
-
+	while ( $str =~m/$substr/g ) {
+    
+		$num++ ;
+        
+	}
+    
     print "$num\n";
+    
 }
 
 1;
