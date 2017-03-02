cdb023d69469d1150a2545e5b17f454500cae6b9
MarinaSolyanikova <Solyanikova_marina@mail.ru>
2017-03-02 10:52:24 +0300

    Update 02.pl

--- homeworks/simple_tasks/bin/02.pl
+++ homeworks/simple_tasks/bin/02.pl
@@ -4,18 +4,17 @@ use strict;
 use warnings;
 
 sub run {
-
-	my ($x, $y) = @_;
-    
+   my ($x, $y) = @_; 
 	for (my $i = $x; $i <= $y; $i++) { 
-	
+
 		if ($x<0) {next;}
-		elsif ($i % 2 and ('Q' x $i)!~m/^(QQ+?)\1+$/ or $i==2) {
-		
-		print "$i\n";	  
-		
-		}
-		
+		elsif ( $i != 1 and $i % 2 and ('Q' x $i)!~m/^(QQ+?)\1+$/ or $i==2) {
+
+		print "$i\n"
+
+		}	     
+
+
 	}
 
 }
