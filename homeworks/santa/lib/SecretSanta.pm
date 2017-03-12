6ce0782511c100ad290af2bc2a52e41ed972f3ff
MarinaSolyanikova <Solyanikova_marina@mail.ru>
2017-03-12 17:12:02 +0300

    Update SecretSanta.pm

--- homeworks/santa/lib/SecretSanta.pm
+++ homeworks/santa/lib/SecretSanta.pm
@@ -4,13 +4,42 @@ use 5.010;
 use strict;
 use warnings;
 use DDP;
+#use List::Util qw/shuffle/;
 
 sub calculate {
 	my @members = @_;
 	my @res;
-	# ...
-	#	push @res,[ "fromname", "toname" ];
-	# ...
+	my @from;
+	my @to;
+	my %ft;
+	#@members = shuffle @members;
+
+
+	foreach (@members) {	
+		if(ref($_) eq 'ARRAY') {
+		push @from, @$_;
+		}
+             	else {
+		push @from, $_;
+		}
+	}
+
+	@to = @from;
+	my $a;	
+	$a = shift @to;
+	push @to, $a;
+	$a = shift @to;
+	push @to, $a;
+	
+	foreach ( 0 .. $#from ) {
+	$ft{$from[$_]} = $to[$_];
+	}
+
+	while ( (my $k, my $v) = each %ft)  {
+	push @res, [$k,$v];
+	}
+
+	print Dumper @res;
 	return @res;
 }
 
